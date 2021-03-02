9271e43926eb9a02420246216bfbe240445742e4
Sahil Ajmeri <sahil.ms1113@gmail.com>
2021-03-02 15:45:14 +0530

    Project Completed and Tested Successfully

---
+++ index.py
@@ -0,0 +1,15 @@
+#importing the pyttsx3 library
+import pyttsx3
+
+#initialisation
+engine=pyttsx3.init()
+
+#testing
+engine.say("Hey Google")
+engine.say("Search for youtube channel Tech Eos ")
+
+engine.say("....... And i would like you to inform that")
+engine.say("This is my first text to speed program")
+engine.say("And everything which i like i will type here will be spoken")
+engine.say("..Thanking you , Yours Faithfully , Sahil Ajmeri")
+engine.runAndWait()
